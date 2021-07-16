58b44dcfed78af808a377be146d8a6b864ff9ffb
hibiki <hibiki123123@gmail.com>
2021-07-16 17:32:07 +0900

    some change client_test.go

--- scrape_client/client/client_test.go
+++ scrape_client/client/client_test.go
@@ -22,24 +22,6 @@ func TestScraping(t *testing.T) {
 		t.Error("len of scrapedResults is 0.")
 	}
 
-<<<<<<< HEAD
-	for _, ele := range scrapedResults {
-		if ele.Dealer == "" || ele.Name == "" || ele.Url == "" || ele.Price == "" || len(ele.RegionList) == 0 || ele.StoreLat < -45 || ele.StoreLat > 45 || ele.StoreLon < -180 || ele.StoreLon > 180 {
-			fmt.Printf("Dealer: %v \n", ele.Dealer)
-			fmt.Printf("Name: %v \n", ele.ProductName)
-			fmt.Printf("Url: %v \n", ele.ProductUrl)
-			fmt.Printf("Price: %v \n", ele.ProductImgUrl)
-			fmt.Printf("RegionList: %v \n", ele.ProductRegionList)
-			fmt.Printf("StoreLat: %v \n", ele.StoreLat)
-			fmt.Printf("StoreLon: %v \n", ele.StoreLon)
-			m := ele.ToMapSpecificFields(true)
-			t.Errorf("This is not full parameta. %v \n", m)
-		}
-	}
-=======
-	log.Printf("scrapedResults: %v \n", scrapedResults)
-
->>>>>>> dev
 	fmt.Println(scrapedResults)
 }
 
